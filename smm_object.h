f8abb3bb11b0365a00181d82674c2cfab8d037f9
JiyunHwang1008 <kuss7732@sookmyung.ac.kr>
2025-12-08 17:10:07 +0900

    6

--- smm_object.h
+++ smm_object.h
@@ -17,6 +17,13 @@
     foodChance,
     festival
 */
+#define SMMNODE_TYPE_LECTURE           0
+#define SMMNODE_TYPE_RESTAURANT        1
+#define SMMNODE_TYPE_LABORATORY        2
+#define SMMNODE_TYPE_HOME              3
+#define SMMNODE_TYPE_GOTOLAB           4
+#define SMMNODE_TYPE_FOODCHANCE        5
+#define SMMNODE_TYPE_FESTIVAL          6
 
 
 /* grade :
@@ -35,10 +42,11 @@
 
 //object generation
 int smmObj_genNode(char* name, int type, int credit, int energy);
-char* smmobj_getName(int node_nr);
-int smmobj_getType(int node_nr);
-int smmobj_getEnergy(int node_nr);
-char* smmobj_getNodeName(int node_type);
+char* smmobj_getNodeName(int node_nr);
+int smmobj_getNodeType(int node_nr);
+int smmobj_getNodeEnergy(int node_nr);
+char* smmobj_getTypeName(int node_type);
+int smmobj_getNodeCredit(int node_nr);
 //member retrieving
 
 
